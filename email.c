a81590d35a02ddc69fff48c428e2ae2559147bf2
Triptych03 <Seaitken@hotmail.com>
2014-08-25 11:19:48 +1000

    Update email.c

--- email.c
+++ email.c
@@ -16,72 +16,71 @@ PG_MODULE_MAGIC;
 
 typedef struct Email
 {
- //struct details here	
-}
+   char local;
+   char domain;   
+}  Email;
 /*
  * Since we use V1 function calling convention, all these functions have
  * the same signature as far as C is concerned.  We provide these prototypes
  * just to forestall warnings when compiled with gcc -Wmissing-prototypes.
  */
-Datum		complex_in(PG_FUNCTION_ARGS);
-Datum		complex_out(PG_FUNCTION_ARGS);
-Datum		complex_recv(PG_FUNCTION_ARGS);
-Datum		complex_send(PG_FUNCTION_ARGS);
-Datum		complex_add(PG_FUNCTION_ARGS);
-Datum		complex_abs_lt(PG_FUNCTION_ARGS);
-Datum		complex_abs_le(PG_FUNCTION_ARGS);
-Datum		complex_abs_eq(PG_FUNCTION_ARGS);
-Datum		complex_abs_ge(PG_FUNCTION_ARGS);
-Datum		complex_abs_gt(PG_FUNCTION_ARGS);
-Datum		complex_abs_cmp(PG_FUNCTION_ARGS);
+Datum		email_in(PG_FUNCTION_ARGS);
+Datum		email_out(PG_FUNCTION_ARGS);
+Datum		email_recv(PG_FUNCTION_ARGS);
+Datum		email_send(PG_FUNCTION_ARGS);
+Datum		email_lt(PG_FUNCTION_ARGS);
+Datum		email_le(PG_FUNCTION_ARGS);
+Datum		email_eq(PG_FUNCTION_ARGS);
+Datum		email_ge(PG_FUNCTION_ARGS);
+Datum		email_gt(PG_FUNCTION_ARGS);
+Datum		email_deq(PG_FUNCTION_ARGS);
+Datum       email_notdeq(PG_FUNCTION_ARGS);
 
 
 /*****************************************************************************
  * Input/Output functions
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(complex_in);
+PG_FUNCTION_INFO_V1(email_in);
 
 Datum
 complex_in(PG_FUNCTION_ARGS)
 {
 	char	   *str = PG_GETARG_CSTRING(0);
-	double		x,
-				y;
-	Complex    *result;
+	char		local,
+				domain;
+	Email      *result;
 
-	if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
+	if (sscanf(str, " ( %c , %c )", &local, &domain) = ?????)
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for complex: \"%s\"",
+				 errmsg("invalid input syntax for Email Address: \"%s\"",
 						str)));
 
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = x;
-	result->y = y;
+	result = (Email *) palloc(sizeof(Email));
+	result->local = local;
+	result->domain = domain;
 	PG_RETURN_POINTER(result);
 }
 
-PG_FUNCTION_INFO_V1(complex_out);
+PG_FUNCTION_INFO_V1(email_out);
 
 Datum
-complex_out(PG_FUNCTION_ARGS)
+email_out(PG_FUNCTION_ARGS)
 {
-	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-	char	   *result;
+	Email    *email = (Email *) PG_GETARG_POINTER(0);
+	char	 *result;
 
 	result = (char *) palloc(100);
-	snprintf(result, 100, "(%g,%g)", complex->x, complex->y);
+	snprintf(result, 100, "(%g, @, %g)", email->local, email->domain);
 	PG_RETURN_CSTRING(result);
 }
 
 /*****************************************************************************
  * Binary Input/Output functions
- *
- * These are optional.
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(complex_recv);
+PG_FUNCTION_INFO_V1(email_recv);
 
 Datum
 complex_recv(PG_FUNCTION_ARGS)
@@ -110,36 +109,7 @@ complex_send(PG_FUNCTION_ARGS)
 }
 
 /*****************************************************************************
- * New Operators
- *
- * A practical Complex datatype would provide much more than this, of course.
- *****************************************************************************/
-
-PG_FUNCTION_INFO_V1(complex_add);
-
-Datum
-complex_add(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-	Complex    *result;
-
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = a->x + b->x;
-	result->y = a->y + b->y;
-	PG_RETURN_POINTER(result);
-}
-
-
-/*****************************************************************************
  * Operator class for defining B-tree index
- *
- * It's essential that the comparison operators and support function for a
- * B-tree index opclass always agree on the relative ordering of any two
- * data values.  Experience has shown that it's depressingly easy to write
- * unintentionally inconsistent functions.	One way to reduce the odds of
- * making a mistake is to make all the functions simple wrappers around
- * an internal three-way-comparison function, as we do here.
  *****************************************************************************/
 
 #define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
