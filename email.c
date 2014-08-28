173b2647a0dfaf7b589413681d4cb6ddf3aae252
Triptych03 <Seaitken@hotmail.com>
2014-08-28 13:04:22 +1000

    Update email.c

--- email.c
+++ email.c
@@ -1,15 +1,15 @@
-/*
- * src/tutorial/email.c
- *
- ******************************************************************************
- COMP9315 Database Systems Imp 
- Stuart Aitken, Gina Jordanova
- Assignment 1 Adding an Email Data Type to PostgreSQL
-******************************************************************************/
+
+// src/tutorial/email.c
+
+//******************************************************************************
+//COMP9315 Database Systems Imp 
+//Stuart Aitken, Gina Jordanova
+//Assignment 1 Adding an Email Data Type to PostgreSQL
+//******************************************************************************
 
 #include "postgres.h"
 #include "fmgr.h"
-#include "libpq/pqformat.h"		/* needed for send/recv functions */
+#include "libpq/pqformat.h"		// needed for send/recv functions
 
 
 PG_MODULE_MAGIC;
@@ -19,27 +19,28 @@ typedef struct Email
    char local;
    char domain;   
 }  Email;
-/*
- * Since we use V1 function calling convention, all these functions have
- * the same signature as far as C is concerned.  We provide these prototypes
- * just to forestall warnings when compiled with gcc -Wmissing-prototypes.
- */
-Datum		email_in(PG_FUNCTION_ARGS);
-Datum		email_out(PG_FUNCTION_ARGS);
-Datum		email_recv(PG_FUNCTION_ARGS);
-Datum		email_send(PG_FUNCTION_ARGS);
-Datum		email_lt(PG_FUNCTION_ARGS);
-Datum		email_le(PG_FUNCTION_ARGS);
-Datum		email_eq(PG_FUNCTION_ARGS);
-Datum		email_ge(PG_FUNCTION_ARGS);
-Datum		email_gt(PG_FUNCTION_ARGS);
-Datum		email_deq(PG_FUNCTION_ARGS);
-Datum       email_notdeq(PG_FUNCTION_ARGS);
+
+//* Since we use V1 function calling convention, all these functions have
+//* the same signature as far as C is concerned.  We provide these prototypes
+//* just to forestall warnings when compiled with gcc -Wmissing-prototypes.
+
+Datum	email_in(PG_FUNCTION_ARGS);
+Datum	email_out(PG_FUNCTION_ARGS);
+Datum	email_recv(PG_FUNCTION_ARGS);
+Datum	email_send(PG_FUNCTION_ARGS);
+Datum	email_lt(PG_FUNCTION_ARGS);
+Datum	email_le(PG_FUNCTION_ARGS);
+Datum	email_eq(PG_FUNCTION_ARGS);
+Datum	email_noteq(PG_FUNCTION_ARGS);
+Datum	email_gt(PG_FUNCTION_ARGS);
+Datum	email_ge(PG_FUNCTION_ARGS);
+Datum	email_deq(PG_FUNCTION_ARGS);
+Datum   email_notdeq(PG_FUNCTION_ARGS);
 
 
-/*****************************************************************************
- * Input/Output functions
- *****************************************************************************/
+//*****************************************************************************
+//* Input/Output functions
+//*****************************************************************************
 
 PG_FUNCTION_INFO_V1(email_in);
 
@@ -51,7 +52,7 @@ email_in(PG_FUNCTION_ARGS)
 				domain;
 	Email      *result;
 
-	if (sscanf(str, " ( %c , %c )", &local, &domain) = ?????)
+	if (sscanf(str, "%s @ %s", &local, &domain) != 2)
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 				 errmsg("invalid input syntax for Email Address: \"%s\"",
@@ -72,46 +73,46 @@ email_out(PG_FUNCTION_ARGS)
 	char	 *result;
 
 	result = (char *) palloc(100);
-	snprintf(result, 100, "(%c, @, %c)", email->local, email->domain);
+	snprintf(result, 100, "%s @ %s", email->local, email->domain);
 	PG_RETURN_CSTRING(result);
 }
 
-/*****************************************************************************
- * Binary Input/Output functions
- *****************************************************************************/
+//*****************************************************************************
+//* Binary Input/Output functions
+//*****************************************************************************
 
 PG_FUNCTION_INFO_V1(email_recv);
 
 Datum
-complex_recv(PG_FUNCTION_ARGS)
+email_recv(PG_FUNCTION_ARGS)
 {
 	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
-	Complex    *result;
+	Email    *result;
 
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = pq_getmsgfloat8(buf);
-	result->y = pq_getmsgfloat8(buf);
+	result = (Email *) palloc(sizeof(Email));
+	result->local = pq_getmsgstring(buf);
+	result->domain = pq_getmsgstring(buf);
 	PG_RETURN_POINTER(result);
 }
 
-PG_FUNCTION_INFO_V1(complex_send);
+PG_FUNCTION_INFO_V1(email_send);
 
 Datum
-complex_send(PG_FUNCTION_ARGS)
+email_send(PG_FUNCTION_ARGS)
 {
-	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
+	Email    *email = (Email *) PG_GETARG_POINTER(0);
 	StringInfoData buf;
 
 	pq_begintypsend(&buf);
-	pq_sendfloat8(&buf, complex->x);
-	pq_sendfloat8(&buf, complex->y);
+	pq_sendstring(&buf, email->local);
+	pq_sendstring(&buf, email->domain);
 	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 }
 
-/*****************************************************************************
- * Operator class for defining B-tree index
- *****************************************************************************/
-
+//*****************************************************************************
+//* Operator class for defining B-tree index
+//*****************************************************************************
+/**
 #define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
 
 static int
@@ -193,3 +194,4 @@ complex_abs_cmp(PG_FUNCTION_ARGS)
 
 	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
 }
+**/
